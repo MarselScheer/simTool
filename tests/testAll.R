c6ee97e48c34b0588b4a24c9462a460458351f91
m <scheer@freescience.de>
2017-07-29 23:18:41 +0200

    runit tests transferred to test_that tests

--- tests/testAll.R
+++
@@ -1,7 +0,0 @@
-require(RUnit)
-require(simTool)
-testsuite.all <- defineTestSuite("ALL", dirs = ".", testFileRegexp = "^runit.+\\.R", testFuncRegexp = "^test.+")#,
-#                                 rngKind = "Marsaglia-Multicarry",
-#                                 rngNormalKind = "Kinderman-Ramage")
-printTextProtocol(rts <- runTestSuite(testsuite.all))
-printHTMLProtocol(rts, fileName="out.html")
--- tests/testthat/test_as.data.frame.evalGrid.R
+++ tests/testthat/test_as.data.frame.evalGrid.R
@@ -20,9 +20,7 @@ test_that("Deprecated parameters value.fun, post.proc from as.data.frame generat
 
 
 
-# check if summary.fun can handle numeric and logical
 f = function(x) x <= 0.5
-
 result = 
   structure(list(
   i = 1:4, j = c(1L, 1L, 1L, 1L), fun = c("runif", "runif", "runif", "runif"), 
@@ -60,97 +58,103 @@ test_that("Variable names are introduced, where the analyzing function does not 
           expect_identical(names(df), c("i", "j", "fun", "n", "proc", "replication", "V1", "min", "V2")))
 
 
-test.as.data.frame.evalGrid = function(){
+
+# -------------------------------------------------------
 
 
-  genRegData <- function(){
-    data.frame(
-      x = 1:10,
-      y = rnorm(10, mean=1:10))
-  }
-  
-  
-  set.seed(19032013)
-  eg <- evalGrids(
-    expandGrid(fun="genRegData"),
-    expandGrid(proc="lm", formula=c("y ~ x", "y ~ x + I(x^2)")),
-    replications=10, envir=environment())
-
-  lm2df = function(lm.object) {
-    ret = coef(summary(lm.object))
-    data.frame(covariable = rownames(ret), ret, check.names=FALSE)
-  }
-  df<-as.data.frame(eg, convert.result.fun=lm2df)  
-  require(plyr)
-  tmp = ldply(eg$simulation[[1]], function(v) rbind(lm2df(v$results[[1]]), lm2df(v$results[[2]])))
-  tmp = arrange(tmp, Estimate)
-  df = arrange(df, Estimate)
-  checkEquals(all(df[,-(1:6)] == tmp), TRUE)
-
-  
-  df = as.data.frame(eg, convert.result.fun=lm2df, summary.fun=mean)
-  require(reshape)
-  tmp = ldply(eg$simulation[[1]], function(v) rbind(lm2df(v$results[[1]])))
-  mtmp = melt(tmp, id=1)
-  tmp1 = cast(mtmp, ... ~ variable, mean)
-  
-  tmp = ldply(eg$simulation[[1]], function(v) rbind(lm2df(v$results[[2]])))
-  mtmp = melt(tmp, id=1)
-  tmp2 = cast(mtmp, ... ~ variable, mean)
-  
-  tmp = rbind(tmp1, tmp2)
-  checkEquals(all(df[,-(1:5)] == tmp), TRUE)
-  
-  
-  df = as.data.frame(eg, convert.result.fun=lm2df, summary.fun=c(mean, sd))
-  
-  tmp = ldply(eg$simulation[[1]], function(v) rbind(lm2df(v$results[[1]])))
-  mtmp = melt(tmp, id=1)
-  tmp1 = cast(mtmp, ... ~ variable, c(mean, sd))
-
-  tmp = ldply(eg$simulation[[1]], function(v) rbind(lm2df(v$results[[2]])))
-  mtmp = melt(tmp, id=1)
-  tmp2 = cast(mtmp, ... ~ variable, c(mean, sd))
-  
-  tmp = rbind(tmp1, tmp2)
-  checkEquals(all(df[,-(1:5)] == tmp), TRUE)  
+genRegData <- function(){
+  data.frame(
+    x = 1:10,
+    y = rnorm(10, mean=1:10))
 }
 
-test.as.data.frame.from.fallback = function(){
-  require(plyr)
-  genData = function(N){N}
-  brichtAb = function(data){    
-    if (data == 3)
-      stop("Bewusster Error: fallBackTest")
-    1
-  }
-  
-  dg = expandGrid(fun=c("genData"), N=1:5)  
-  pg = expandGrid(proc=c("brichtAb"))  
-  if(is.element("RUnitFallBack.Rdata", dir()))
-    file.remove("RUnitFallBack.Rdata")
-  
-  checkException(eg <- evalGrids(dg, pg, replications=4, progress=TRUE, fallback="RUnitFallBack", envir=environment()))
-  rm(list=ls())
-  load("RUnitFallBack.Rdata")
-  df = as.data.frame(fallBackObj)
-  
-  # dput
-  df2 = structure(list(
-    i = c(1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 3L, 4L, 5L), 
-    j = c(1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L), 
-    fun = c("genData", "genData", "genData", "genData", 
-            "genData", "genData", "genData", "genData", "genData", "genData", "genData"), 
-    N = c(1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 3L, 4L, 5L), 
-    proc = c("brichtAb", "brichtAb", "brichtAb", "brichtAb", 
-             "brichtAb", "brichtAb", "brichtAb", "brichtAb", 
-             "brichtAb", "brichtAb", "brichtAb"), 
-    replication = structure(c(1L, 2L, 3L, 4L, 1L, 2L, 3L, 4L, NA, NA, NA), 
-                            .Label = c("1", "2", "3", "4"), class = "factor"), 
-    V1 = c(1, 1, 1, 1, 1, 1, 1, 1, NA, NA, NA), 
-    .evalGridComment = structure(c(NA, NA, NA, NA, NA, NA, NA, NA, 1L, 1L, 1L), 
-                                 .Label = "Results missing", class = "factor")),
-    .Names = c("i", "j", "fun", "N", "proc", "replication", "V1", ".evalGridComment"), 
-    row.names = c(NA, -11L), class = "data.frame")
-  checkEquals(df, df2)
+
+set.seed(19032013)
+eg <- evalGrids(
+  expandGrid(fun="genRegData"),
+  expandGrid(proc="lm", formula=c("y ~ x", "y ~ x + I(x^2)")),
+  replications=10, envir=environment())
+
+lm2df = function(lm.object) {
+  ret = coef(summary(lm.object))
+  data.frame(covariable = rownames(ret), ret, check.names=FALSE)
 }
+df<-as.data.frame(eg, convert.result.fun=lm2df)  
+
+require(plyr)
+tmp = ldply(eg$simulation[[1]], function(v) rbind(lm2df(v$results[[1]]), lm2df(v$results[[2]])))
+tmp = arrange(tmp, Estimate)
+df = arrange(df, Estimate)
+
+test_that("Check that results can be converted to a data.frame", 
+          expect_identical(df[,-(1:6)], tmp))
+
+
+df = as.data.frame(eg, convert.result.fun=lm2df, summary.fun=mean)
+require(reshape)
+tmp = ldply(eg$simulation[[1]], function(v) rbind(lm2df(v$results[[1]])))
+mtmp = melt(tmp, id=1)
+tmp1 = cast(mtmp, ... ~ variable, mean)
+
+tmp = ldply(eg$simulation[[1]], function(v) rbind(lm2df(v$results[[2]])))
+mtmp = melt(tmp, id=1)
+tmp2 = cast(mtmp, ... ~ variable, mean)
+tmp = rbind(tmp1, tmp2)
+test_that("Check that results can be converted to a data.frame and summarized over the replications", 
+          expect_equivalent(df[,-(1:5)], tmp))
+
+df = as.data.frame(eg, convert.result.fun=lm2df, summary.fun=c(mean, sd))
+
+tmp = ldply(eg$simulation[[1]], function(v) rbind(lm2df(v$results[[1]])))
+mtmp = melt(tmp, id=1)
+tmp1 = cast(mtmp, ... ~ variable, c(mean, sd))
+
+tmp = ldply(eg$simulation[[1]], function(v) rbind(lm2df(v$results[[2]])))
+mtmp = melt(tmp, id=1)
+tmp2 = cast(mtmp, ... ~ variable, c(mean, sd))
+
+tmp = rbind(tmp1, tmp2)
+test_that("Check that results can be converted to a data.frame and summarized (vector) over the replications", 
+          expect_equivalent(df[,-(1:5)], tmp))
+
+
+require(plyr)
+genData = function(N){N}
+brichtAb = function(data){    
+  if (data == 3)
+    stop("Bewusster Error: fallBackTest")
+  1
+}
+
+dg = expandGrid(fun=c("genData"), N=1:5)  
+pg = expandGrid(proc=c("brichtAb"))  
+if(is.element("RUnitFallBack.Rdata", dir("./tests/testthat/")))
+  file.remove("./tests/testthat/RUnitFallBack.Rdata")
+
+test_that("Create an error to check afterwards that the results so far were stored",
+          expect_error(eg <- evalGrids(dg, pg, replications=4, progress=TRUE, fallback="RUnitFallBack", envir=environment())))
+rm(list=ls())
+
+load("RUnitFallBack.Rdata")
+df = as.data.frame(fallBackObj)
+
+# dput
+df2 = structure(list(
+  i = c(1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 3L, 4L, 5L), 
+  j = c(1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L), 
+  fun = c("genData", "genData", "genData", "genData", 
+          "genData", "genData", "genData", "genData", "genData", "genData", "genData"), 
+  N = c(1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 3L, 4L, 5L), 
+  proc = c("brichtAb", "brichtAb", "brichtAb", "brichtAb", 
+           "brichtAb", "brichtAb", "brichtAb", "brichtAb", 
+           "brichtAb", "brichtAb", "brichtAb"), 
+  replication = structure(c(1L, 2L, 3L, 4L, 1L, 2L, 3L, 4L, NA, NA, NA), 
+                          .Label = c("1", "2", "3", "4"), class = "factor"), 
+  V1 = c(1, 1, 1, 1, 1, 1, 1, 1, NA, NA, NA), 
+  .evalGridComment = structure(c(NA, NA, NA, NA, NA, NA, NA, NA, 1L, 1L, 1L), 
+                               .Label = "Results missing", class = "factor")),
+  .Names = c("i", "j", "fun", "N", "proc", "replication", "V1", ".evalGridComment"), 
+  row.names = c(NA, -11L), class = "data.frame")
+
+test_that("Fallback object was correctly stored to hard drive", 
+          expect_identical(df, df2))
