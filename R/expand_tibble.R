f93967e3b9aa1eb5efcb3ac360626693f4e515e6
m <scheer@freescience.de>
2017-09-23 12:17:26 +0200

    expand_tibble() added

---
+++ R/expand_tibble.R
@@ -0,0 +1,22 @@
+#' Creates a \code{tibble} from All Combinations
+#'
+#'  Actually a wrapper for \code{\link{expand.grid}}, but
+#'  character vectors will stay as characters.
+#'
+#'
+#'@param \dots  vectors, factors or a list containing these.
+#'@return See \code{\link{expand.grid}} but instead of a \code{\link{data.frame}}
+#'  a \code{\link{tibble}} is returned.
+#'@author  Marsel Scheer
+#'@seealso  \code{\link{expand.grid}}
+#'@examples
+#'
+#'expand_tibble(fun="rnorm", mean=1:4, sd=2:5)
+#'
+#'@importFrom tibble as_tibble
+#'@export
+expand_tibble <-
+  function(...){
+    expand.grid(..., stringsAsFactors=FALSE) %>% 
+      tibble::as_tibble()
+  }
