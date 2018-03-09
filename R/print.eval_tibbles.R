2821d215fd68994625607cc12d362e362037ab97
m <scheer@freescience.de>
2018-03-09 21:46:47 +0100

    print method for class eval_tibbles added

---
+++ R/print.eval_tibbles.R
@@ -0,0 +1,17 @@
+#' Printing simulation results
+#'
+#'  Prints objects created by \code{eval_tibbles()}
+#'
+#'
+#'@param x  object of class \code{eval_tibbles}
+#'@author  Marsel Scheer
+#'@export
+print.eval_tibbles = function(x){
+  print(x$simulation)
+  cat(sprintf("Number of data generating functions: %i\n", nrow(x$data_grid)))
+  cat(sprintf("Number of analyzing procedures: %i\n", nrow(x$proc_grid)))
+  cat(sprintf("Number of replications: %i\n", x$replications))
+  cat(sprintf("Estimated replications per hour: %s\n", x$est_reps_per_hour))
+  cat(sprintf("Start of the simulation: %s\n", x$start_time))
+  cat(sprintf("End of the simulation: %s\n", x$end_time))
+}
