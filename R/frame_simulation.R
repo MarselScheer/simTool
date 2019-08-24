dcabca7aee9e480cc8f99d6413287b4ab92eab2c
m <scheer@freescience.de>
2019-08-24 08:31:53 +0200

    comment corrected

--- R/frame_simulation.R
+++ R/frame_simulation.R
@@ -1,7 +1,7 @@
 #' @importFrom tidyr crossing
 #' @importFrom purrr flatten
 frame_simulation <- function(dg, pg, sim, summary_fun) {
-  # if summary_fun is NULL, names(summary_fun) is NULL and crossing ignores it.
+  # if summary_fun is NULL, names(summary_fun) is NULL and expand_grid ignores it.
   ret <- tidyr::expand_grid(dg,
     replications = seq_along(sim[[1]]),
     summary_fun = names(summary_fun), pg
