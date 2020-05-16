4fd9c02bf34ef63f831fd6ad59cb289dc2069a9e
m <scheer@freescience.de>
2020-05-16 10:46:38 +0200

    correct docker-start-script

--- docker/start.sh
+++ docker/start.sh
@@ -1,1 +1,1 @@
-sudo docker run -d -p 8787:8787 -e DISABLE_AUTH=true -v ~/docker_fs:/tmp/hostfs rstudio simtool_ide
+sudo docker run -d -p 8787:8787 -e DISABLE_AUTH=true -v ~/docker_fs:/tmp/hostfs simtool_ide:4.0.0
