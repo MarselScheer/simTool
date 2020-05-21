4a33a49f72c38954b9a7ace3d901a2363e51e145
Marsel Scheer <scheer@freescience.de>
2020-05-21 17:22:26 +0200

    update Dockerfile use new theme for emacs
    - set also TERM-environment so that theme
      is correctly displayed in the terminal

--- docker/start.sh
+++ docker/start.sh
@@ -1,1 +1,1 @@
-sudo docker run -d -p 8787:8787 -e DISABLE_AUTH=true -v ~/docker_fs:/tmp/hostfs simtool_ide:4.0.0
+sudo docker run -d -p 8787:8787 -e DISABLE_AUTH=true -e TERM=xterm-256color -v ~/docker_fs:/tmp/hostfs simtool_ide:4.0.0
