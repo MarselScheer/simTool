ac84bac28e93a6ecd01982de0a81fb7b059a97da
Marsel Scheer <scheer@freescience.de>
2020-08-29 10:23:23 +0000

    transfer tests for expand_tibble to tinytest

---
+++ inst/tinytest/test_expand_tibble.R
@@ -0,0 +1,23 @@
+#-# crossproduct of complicated objects
+cross_product_of_complicated_objects <- function() {
+  tmp <- expand_tibble(
+    desc = letters[1:2],
+    mod = list(
+      lm(Sepal.Length ~ Sepal.Width, data = iris),
+      lm(Sepal.Length ~ Petal.Width, data = iris)
+    )
+  )
+
+  er <- dplyr::tibble(
+    desc = c("a", "b", "a", "b"),
+    mod = list(
+      lm(Sepal.Length ~ Sepal.Width, data = iris),
+      lm(Sepal.Length ~ Sepal.Width, data = iris),
+      lm(Sepal.Length ~ Petal.Width, data = iris),
+      lm(Sepal.Length ~ Petal.Width, data = iris)
+    )
+  )
+  expect_equal(tmp$desc, er$desc)
+  expect_equal(tmp$mod, er$mod, info = "expand_tibble also works with model fits")
+}
+cross_product_of_complicated_objects()
