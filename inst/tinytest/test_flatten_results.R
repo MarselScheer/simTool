01c5dc6e780eee79dabf249673c2ea4752f64dd3
Marsel Scheer <scheer@freescience.de>
2020-08-29 11:25:01 +0000

    add namespace for testing non-exported function

--- inst/tinytest/test_flatten_results.R
+++ inst/tinytest/test_flatten_results.R
@@ -5,7 +5,7 @@ extract_if_only_one_result_is_available <- function() {
       list(list(data = "2. set", results = list(c(a = 3, b = 4))))
     )
   expect_identical(
-    flatten_results(data_and_results),
+    simTool:::flatten_results(data_and_results),
     list(c(a = 1, b = 2), c(a = 3, b = 4))
   )
 }
@@ -20,7 +20,7 @@ extract_if_more_than_one_result_is_available <- function() {
         results = list(c(a = 3, b = 4), 20, matrix(2, 2, 2))))
     )
   expect_identical(
-    flatten_results(data_and_results),
+    simTool:::flatten_results(data_and_results),
     list(
       c(a = 1, b = 2), 10, matrix(1, 2, 2),
       c(a = 3, b = 4), 20, matrix(2, 2, 2)
