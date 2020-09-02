bbb72bb07fb03b0de41c8629347cb94687ade8ec
Marsel Scheer <scheer@freescience.de>
2020-09-02 20:14:21 +0000

    add roxygen docu to Simulation_unit

--- R/Simulation_unit.R
+++ R/Simulation_unit.R
@@ -1,7 +1,24 @@
+#' R6 Class used to define which Data_generator is
+#' used with which Data_analyser
+#'
 #' @import data.table
 #' @export
 Simulation_unit <- R6::R6Class("Eval_suite",
   public = list(
+    ##' @description
+    ##' Creates a new Simulation_unit object.
+    ##' @param generator
+    ##' @param generator a named list with one element containing
+    ##' a \link{Data_generator}
+    ##' @param analysers a named list where every element must be
+    ##' a \link{Data_analyser} that is able to process the output
+    ##' Data_generator passed to \code{generator}
+    ##' @param replications number of replications to be performed
+    ##' @param aggregate_funs (optional) a named list of functions
+    ##' where every function aggregates the replications to one
+    ##' value. Of course the aggregation happens stratified for
+    ##' every analyser.
+    ##' @return A new Simulation_unit object
     initialize = function(generator, analysers, replications,
                           aggregate_funs = NULL) {
       private$generator <- generator
@@ -9,6 +26,25 @@ Simulation_unit <- R6::R6Class("Eval_suite",
       private$replications <- replications
       private$aggregate_funs <- aggregate_funs
     },
+    ##' @description
+    ##' Generates a data set using the Data_generator and
+    ##' pass it to every Data_analyser. This is repeated
+    ##' as often as defined during constructing the object.
+    ##' Furhtermore, the replications are aggregated
+    ##' according to the definition during construction of the
+    ##' object
+    ##' @return a data.table with column
+    ##' \item{generator}{name for the generator
+    ##  defined during construction of the object}
+    ##' \item{analyser}{name for the analyser
+    ##  defined during construction of the object}
+    ##' \item{replication}{number of the replication. Note this
+    ##' column is missing if aggregation was defined}
+    ##' \item{result}{output of the corresponding analyser or
+    ##  the aggregated output over all replications if
+    ##' aggregation was defined. In the case of aggregation
+    ##' it is possible that also the column name result was
+    ##' substitute by the corresponding output of the analyser}
     run_evaluation = function() {
       reps <- seq_len(length.out = private$replications)
       ret <- data.table::data.table(
@@ -41,6 +77,13 @@ Simulation_unit <- R6::R6Class("Eval_suite",
     analysers = NULL,
     replications = NULL,
     aggregate_funs = NULL,
+    ##' @description
+    ##' aggregates the raw replication according to the user
+    ##' defined aggregation function
+    ##' @param result_frame
+    ##' @return if no aggregation is defined \code{result_frame} is returned
+    ##' Otherwise see \link{run_evaluation}, where column replication was
+    ##' dropped.
     aggregate = function(result_frame) {
       if (is.null(private$aggregate_funs)) {
         return(result_frame)
