2815eb3c93e315723c99e00548b2f748f4ca9ec2
Marsel Scheer <scheer@freescience.de>
2020-09-06 08:42:22 +0000

    test simplified for unnest_simulation()

--- inst/tinytest/test_unnest_simulation.R
+++ inst/tinytest/test_unnest_simulation.R
@@ -1,113 +1,57 @@
 simple_unnesting <- function() {
-  s <- simTool::expand_tibble(a = 1:2, b = 3, results = list(r1 = 1L, r2 = 2L))
-  e <- list(simulation = s)
-  expected_df <- list(simulation = structure(list(a = c(1L, 2L, 1L, 2L), b = c(
-    3,
-    3, 3, 3
-  ), results = c(1L, 1L, 2L, 2L)), out.attrs = list(dim = c(
-    a = 2L,
-    b = 1L, results = 2L
-  ), dimnames = list(
-    a = c("a=1", "a=2"), b = "b=3",
-    results = c("results=1", "results=2")
-  )), row.names = c(
-    NA,
-    -4L
-  ), class = c("tbl_df", "tbl", "data.frame")))
 
-  expect_equivalent(simTool:::unnest_simulation(e), expected_df)
+  s <- expand_tibble(a = 1:2, b = 3, results = list(r1 = 1L, r2 = 2L))
+  e <- list(simulation = s)
+  expected_df <- s
+  expected_df$results <- c(1, 1, 2, 2)
+  expect_equivalent(
+    list(simulation = expected_df),
+    simTool:::unnest_simulation(e))
+
 }
 simple_unnesting()
 
 unnesting_with_further_matrices_in_the_row <- function() {
+
   s <- simTool::expand_tibble(
     a = 1:2,
     df = list(matrix(1:6, 3, 2), matrix(1:8, 4, 2)),
     results = list(r1 = 1L, r2 = 2L))
   e <- list(simulation = s)
+  expected_df <- s
+  expected_df$results <- c(1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L)
+  expect_equivalent(
+    list(simulation = expected_df),
+    simTool:::unnest_simulation(e)
+  )
 
-  expected_df <- structure(list(simulation = structure(list(a = c(
-    1L, 2L, 1L,
-    2L, 1L, 2L, 1L, 2L
-  ), df = list(structure(1:6, .Dim = c(3L, 2L)),
-    structure(1:6, .Dim = c(3L, 2L)),
-    structure(1:8, .Dim = c(
-      4L,
-      2L
-    )), structure(1:8, .Dim = c(4L, 2L)), structure(1:6, .Dim = c(
-      3L,
-      2L
-    )), structure(1:6, .Dim = c(3L, 2L)), structure(1:8, .Dim = c(
-      4L,
-      2L
-    )), structure(1:8, .Dim = c(4L, 2L))), results = c(
-      1L, 1L,
-      1L, 1L, 2L, 2L, 2L, 2L
-    )), class = c("tbl_df", "tbl", "data.frame"),
-  row.names = c(NA, -8L), .Names = c("a", "df", "results"))),
-  .Names = "simulation")
-
-  expect_identical(simTool:::unnest_simulation(e), expected_df)
 }
 unnesting_with_further_matrices_in_the_row()
 
 unnesting_with_further_matrices_in_the_row_and_result_itself_contains_data.frames <- function() {
+
   s <- simTool::expand_tibble(
     a = 1:2,
     df = list(matrix(1:6, 3, 2), matrix(1:8, 4, 2)),
     results = list(r1 = as.data.frame(matrix(1:4, 2, 2)),
       r2 = as.data.frame(matrix(1:9, 3, 3))))
   e <- list(simulation = s)
-  expected_df <- structure(list(simulation = structure(list(a = c(
-    1L, 1L, 2L,
-    2L, 1L, 1L, 2L, 2L, 1L, 1L, 1L, 2L, 2L, 2L, 1L, 1L, 1L, 2L, 2L,
-    2L
-  ), df = list(structure(1:6, .Dim = c(3L, 2L)), structure(1:6, .Dim = c(
-    3L,
-    2L
-  )), structure(1:6, .Dim = c(3L, 2L)), structure(1:6, .Dim = c(
-    3L,
-    2L
-  )), structure(1:8, .Dim = c(4L, 2L)), structure(1:8, .Dim = c(
-    4L,
-    2L
-  )), structure(1:8, .Dim = c(4L, 2L)), structure(1:8, .Dim = c(
-    4L,
-    2L
-  )), structure(1:6, .Dim = c(3L, 2L)), structure(1:6, .Dim = c(
-    3L,
-    2L
-  )), structure(1:6, .Dim = c(3L, 2L)), structure(1:6, .Dim = c(
-    3L,
-    2L
-  )), structure(1:6, .Dim = c(3L, 2L)), structure(1:6, .Dim = c(
-    3L,
-    2L
-  )), structure(1:8, .Dim = c(4L, 2L)), structure(1:8, .Dim = c(
-    4L,
-    2L
-  )), structure(1:8, .Dim = c(4L, 2L)), structure(1:8, .Dim = c(
-    4L,
-    2L
-  )), structure(1:8, .Dim = c(4L, 2L)), structure(1:8, .Dim = c(
-    4L,
-    2L
-  ))), V1 = c(
-    1L, 2L, 1L, 2L, 1L, 2L, 1L, 2L, 1L, 2L, 3L, 1L,
-    2L, 3L, 1L, 2L, 3L, 1L, 2L, 3L
-  ), V2 = c(
-    3L, 4L, 3L, 4L, 3L, 4L,
-    3L, 4L, 4L, 5L, 6L, 4L, 5L, 6L, 4L, 5L, 6L, 4L, 5L, 6L
-  ), V3 = c(
-    NA,
-    NA, NA, NA, NA, NA, NA, NA, 7L, 8L, 9L, 7L, 8L, 9L, 7L, 8L, 9L,
-    7L, 8L, 9L
-  )), class = c("tbl_df", "tbl", "data.frame"), row.names = c(
-    NA,
-    -20L
-  ), .Names = c("a", "df", "V1", "V2", "V3"))), .Names = "simulation")
 
-  expect_identical(simTool:::unnest_simulation(e), expected_df)
+  expected_df <- s
+  expected_df$id <- 1:nrow(expected_df)
+  results <- purrr::map_dfr(split(expected_df, expected_df$id), function(sub) {
+    ret <- sub$results[[1]]
+    ret$id <- sub$id
+    ret
+  })
+  expected_df <- dplyr::left_join(expected_df, results, by = "id")
+  expected_df$results <- NULL
+  expected_df$id <- NULL
+  expected_df
+  expect_equivalent(
+    list(simulation = expected_df),
+    simTool:::unnest_simulation(e))
+
 }
 
 unnesting_with_further_matrices_in_the_row_and_result_itself_contains_data.frames()
