9609947592d3c439d05aca34f880a20b311c6a11
Marsel Scheer <scheer@freescience.de>
2020-09-08 20:06:02 +0000

    data generator has short_name for identification

--- R/Data_generator.R
+++ R/Data_generator.R
@@ -5,9 +5,21 @@ Data_generator <- R6::R6Class("Data_generator",
     ##' @description
     ##' Creates a new Data_generator
     ##' @param generator_fun function that generates the data
+    ##' @param short_name string naming the data generator. If
+    ##' not provided a name is generated that consists of the
+    ##' argument passed to \code{generator_fun} and an integer
+    ##' which are the seconds passed since 1970.
     ##' @return no explicit return-value
-    initialize = function(generator_fun) {
+    initialize = function(generator_fun, short_name = NULL) {
       private$generator_fun = generator_fun
+      if (is.null(short_name)) {
+        short_name <- paste0(
+          deparse(substitute(generator_fun)),
+          "_",
+          format(x = Sys.time(), format = "%s")
+          )
+      }
+      private$short_name <- short_name
     },
     ##' @description
     ##' getter for the data generating function
@@ -21,9 +33,16 @@ Data_generator <- R6::R6Class("Data_generator",
     generator = function() {
       f <- self$get_generator()
       return(f())
+    },
+    ##' @description
+    ##' getter for the short_name
+    ##' @return short_name for the data generator
+    get_short_name = function() {
+      return(private$short_name)
     }
   ),
   private = list(
-    generator_fun = NULL
+    generator_fun = NULL,
+    short_name = NULL
   )
 )
--- inst/tinytest/test_generating_data.R
+++ inst/tinytest/test_generating_data.R
@@ -5,3 +5,27 @@ datagen_instance_can_store_data_generating_function <- function() { # nolint
   expect_equal(dg$generator(), 42)
 }
 datagen_instance_can_store_data_generating_function()
+
+datagen_creates_default_shortname <- function() {
+
+  dg <- Data_generator$new(generator_fun = runif)
+  sn <- strsplit(x = dg$get_short_name(), split = "_")[[1]]
+  expect_equal("runif", sn[1])
+  expect_true(difftime(
+    time1 = Sys.Date(),
+    time2 = as.Date(sn[2], format = "%s"),
+    units = "days") < 1)
+
+}
+datagen_creates_default_shortname()
+
+datagen_stores_shortname <- function() {
+
+  short_name <- "abcd"
+  dg <- Data_generator$new(
+    generator_fun = runif,
+    short_name = short_name)
+  expect_equal(dg$get_short_name(), short_name)
+
+}
+datagen_stores_shortname()
