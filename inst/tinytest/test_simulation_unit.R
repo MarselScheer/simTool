0a710f000446ef2c23a6cfa03b45c7fad591d7fc
Marsel Scheer <scheer@freescience.de>
2020-09-15 20:10:00 +0000

    restrict unittests for simulation_unit to a minimum

--- inst/tinytest/test_simulation_unit.R
+++ inst/tinytest/test_simulation_unit.R
@@ -1,5 +1,9 @@
-call_generator_and_analyser <- function() {
-  dg1 <- Data_generator$new(generator_fun = function() return(1:3))
+call_generator_once_then_analyser <- function() {
+  OFFSET <- 0
+  dg1 <- Data_generator$new(generator_fun = function() {
+    OFFSET <<- OFFSET + 1
+    return(OFFSET + 0:2)
+    })
   da1 <- Data_analyser$new(analyser_fun = function(data) min(data))
   da2 <- Data_analyser$new(analyser_fun = function(data) max(data))
   su1 <- Simulation_unit$new(
@@ -7,20 +11,30 @@ call_generator_and_analyser <- function() {
     analysers = list(da1 = da1, da2 = da2),
     replications = 2
   )
+  su1$run_evaluation()
   expect_equivalent(
-    current = su1$run_evaluation(),
-    target = data.table::data.table(
-      generator = "dg1",
-      analyser = c("da1", "da2", "da1", "da2"),
-      replication = c(1, 1, 2, 2),
-      result = list(1, 3, 1, 3)
-    )
+    current = su1$get_results()[["result"]],
+    # first rep dg1 -> da1/2, then second rep dg1 -> da1/2
+    target = list(1, 3, 2, 4)
   )
-  expect_equivalent(
-    current = su1$run_evaluation(),
+}
+call_generator_once_then_analyser()
+
+simulation_unit_stores_results <- function() {
+  dg1 <- Data_generator$new(generator_fun = function() return(1:3))
+  da1 <- Data_analyser$new(analyser_fun = function(data) min(data))
+  da2 <- Data_analyser$new(analyser_fun = function(data) max(data))
+  su1 <- Simulation_unit$new(
+    generator = list(dg1 = dg1),
+    analysers = list(da1 = da1, da2 = da2),
+    replications = 1
+  )
+  out <- su1$run_evaluation()
+  expect_equal(
+    current = out,
     target = su1$get_results())
 }
-call_generator_and_analyser()
+simulation_unit_stores_results()
 
 post_process_after_analysing <- function() {
   dg1 <- Data_generator$new(generator_fun = function() return(1:3))
@@ -33,15 +47,12 @@ post_process_after_analysing <- function() {
     analysers = list(da1 = da1, da2 = da2),
     replications = 1
   )
+  su1$run_evaluation()
   expect_equivalent(
-    current = su1$run_evaluation(),
-    target = data.table::data.table(
-      generator = "dg1",
-      analyser = c("da1", "da2"),
-      replication = c(1, 1),
-      result = list(
+    current = su1$get_results()[["result"]],
+    target = list(
         data.frame(out = 1),
-        data.frame(out = 3))
+        data.frame(out = 3)
     )
   )
 }
@@ -57,15 +68,14 @@ aggregate_results_wo_post_process <- function() {
     replications = 2,
     aggregate_funs = list(mean = mean, sd = sd)
   )
-  expect_equivalent(
-    current = su1$run_evaluation(),
-    target = data.table::data.table(
-      generator = "dg1",
-      analyser = c("da1", "da2"),
-      result_mean = c(1, 3),
-      result_sd = c(0, 0),
-      key = c("generator", "analyser")
-    )
+  su1$run_evaluation()
+  expect_equal(
+    current = su1$get_results()[["result_mean"]],
+    target = c(1, 3)
+  )
+  expect_equal(
+    current = su1$get_results()[["result_sd"]],
+    target = c(0, 0)
   )
 }
 aggregate_results_wo_post_process()
@@ -84,21 +94,39 @@ aggregate_results_w_post_process <- function() { # nolint
     replications = 2,
     aggregate_funs = list(mean = mean, sd = sd)
   )
-  expect_equivalent(
-    current = su1$run_evaluation(),
-    target = data.table::data.table(
-      generator = "dg1",
-      analyser = c("da1", "da2"),
-      min_mean = c(1, NA),
-      max_mean = c(4, NA),
-      sum_mean = c(NA, 10),
-      prod_mean = c(NA, 24),
-      min_sd = c(0, NA),
-      max_sd = c(0, NA),
-      sum_sd = c(NA, 0),
-      prod_sd = c(NA, 0),
-      key = c("generator", "analyser")
-    )
+  su1$run_evaluation()
+  expect_equal(
+    current = su1$get_results()[["min_mean"]],
+    target = c(1, NA)
   )
+  expect_equal(
+    current = su1$get_results()[["max_mean"]],
+    target = c(4, NA)
+  )
+  expect_equal(
+    current = su1$get_results()[["sum_mean"]],
+    target = c(NA, 10)
+  )
+  expect_equal(
+    current = su1$get_results()[["prod_mean"]],
+    target = c(NA, 24)
+  )
+  expect_equal(
+    current = su1$get_results()[["min_sd"]],
+    target = c(0, NA)
+  )
+  expect_equal(
+    current = su1$get_results()[["max_sd"]],
+    target = c(0, NA)
+  )
+  expect_equal(
+    current = su1$get_results()[["sum_sd"]],
+    target = c(NA, 0)
+  )
+  expect_equal(
+    current = su1$get_results()[["prod_sd"]],
+    target = c(NA, 0)
+  )
+
 }
 aggregate_results_w_post_process()
